9960c48b12c9b6fef875a5238fb7d001f59d9eb4
Matcha <soniamuzemil@gmail.com>
2019-06-25 22:58:08 -0400

    created invite command

---
+++ commands/invite.js
@@ -0,0 +1,3 @@
+exports.run = (client, msg, args) => {
+  msg.channel.send("Invite Mad Libz bot: https://discordapp.com/oauth2/authorize?client_id=593240582088818688&scope=bot&permissions=68608");
+}
