2a62f4dc8de0d94115429397b2a1d9438f845608
Matcha <soniamuzemil@gmail.com>
2019-06-26 22:20:47 -0400

    fixed basic start command

--- commands/start.js
+++ commands/start.js
@@ -1,9 +1,5 @@
 exports.run = (client, msg, args) => {
 
-  msg.channel.send("Retrieving a mad lib...")
-  .then(console.log("sent"))
-  .catch(console.error);
-
   const libs = client.fs.readdirSync("./templates/");
   let length = libs.length;
   // get a random mad lib
@@ -11,15 +7,7 @@ exports.run = (client, msg, args) => {
   // the actual mad lib
   let libPath = `./templates/${libs[rng]}`;
 
-  let lib = client.fs.readFileSync(libPath);
-
-  console.log(typeof(lib));
-
-
-  msg.channel.send("Retrieved a Mad Lib template.")
-  .then(console.log("sent"))
-  .catch(console.error);
-
+  let lib = client.fs.readFileSync(libPath, "utf8");
 
   // extract fillers from mad libs game
   let start = 0;
@@ -28,7 +16,7 @@ exports.run = (client, msg, args) => {
   while (lib.indexOf("[", start) > 0) {
     let begin = lib.indexOf("[", start);
     let end = lib.indexOf("]", start) + 1;
-    let word = lib.substring(start, end);
+    let word = lib.substring(begin, end);
     fillers.push(word);
     start = end;
   }
@@ -36,7 +24,9 @@ exports.run = (client, msg, args) => {
   // create a new message collector
   var collector = new client.discord.MessageCollector(msg.channel, m => m.author.id === msg.author.id, {time: 10000000});
   // start the collector
-  msg.channel.send("Welcome to Mad Libz! Please note that only one person can control the bot at a time. Do you understand? Type \"yes\" to confirm.");
+  msg.channel.send("Welcome to Mad Libz! Please note that only one person can control the bot at a time. Do you understand? Type \"yes\" to confirm.")
+  .then(console.log("sent"))
+  .catch(console.error);
 
   let forceEnd;
   let i = -1;
@@ -45,29 +35,19 @@ exports.run = (client, msg, args) => {
   collector.on("collect", m => {
     // if the user does not confirm the game
     // if the user forces the game to end
-    if (m.content === "end") {
-      m.channel.send("Game has been quit")
-      .then(console.log("sent"))
-      .catch(console.error);
-      collector.stop();
+    if (m.content === "end" || (i < 0 && m.content !== "yes")) {
       forceEnd = true;
+      collector.stop();
     }
     else {
-      if (i < 0 && m.content !== "yes") {
-        m.channel.send("Game has been quit")
-        .then(console.log("sent"))
-        .catch(console.error);
-        collector.stop();
-      }
-      // if the user does confirm the game
-      else if (i < 0 && m.content === null) {
+     if (i < 0) {
         i++;
         m.channel.send(`Please give a **${fillers[i].substring(1, fillers[i].length - 1)}**.`)
         .then(console.log("sent"))
         .catch(console.error);
       }
       // if the user plays as normal
-      else if (i < fillers.length) {
+      else if (i < fillers.length - 1) {
         i++;
         words.push(m.content);
         m.channel.send(`Please give a **${fillers[i].substring(1, fillers[i].length - 1)}**.`)
@@ -83,19 +63,29 @@ exports.run = (client, msg, args) => {
   });
 
   // if the game finishes and the user did not force the game to end
-  if (!forceEnd) {
-    msg.channel.send("*Generating Mad Lib story...*")
-    .then(m => {
-      for (var i = 0; i < words.length; i++) {
-        lib.replace(fillers[i], words[i]);
+  collector.on("end", collected => {
+    if (forceEnd) {
+      msg.channel.send("Game has been quit")
+      .then(console.log("quit game"))
+      .catch(console.error);
+    }
+    else {
+      let toDel;
+      msg.channel.send("Okay, generating story...")
+      .then(m => {
+        toDel = m;
+      })
+      .catch(console.error);
+      console.log("fillers: ", fillers);
+      for (var i = 0; i < fillers.length; i++) {
+        console.log("looping");
+        lib = lib.replace(fillers[i], words[i]);
       }
-
-      m.delete();
-    })
-    .catch(console.error);
-
-    msg.channel.send(`Here is your story: \n ${lib}`)
-    .then(console.log("sent"))
-    .catch(console.error);
-  }
+      msg.channel.send(`Here is your story: \n ${lib}`)
+      .then(m => {
+        toDel.delete();
+      })
+      .catch(console.error);
+    }
+  });
 }
